0562a3dc6054bec9b0c9a3fc10372782521a9386
Arnaud <mrandriamihaja2@gmail.com>
2020-11-13 23:21:56 +0300

    installation vue axios

---
+++ app/javascript/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <div v-for="(hotel, index) in hotels" :key="index" >
+      {{ hotel.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue God!",
+      hotels:[]
+    }
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:3000/v1/hotels')
+      .then(response => {
+        this.hotels = response.data
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+  }
+
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
