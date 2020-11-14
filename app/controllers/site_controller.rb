79b2c4a1bac758ce679f1abf53d504dbd943253a
Arnaud <mrandriamihaja2@gmail.com>
2020-11-14 09:44:50 +0300

    stable

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,7 @@ class SiteController < ApplicationController
   def index
   
   end
+
+  def login
+  end
 end
---
+++ app/javascript/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app" class="login" >
+        <input type="text" placeholder="Email" >
+        <input type="text" placeholder="Mots de pass" >
+        <button  > connecter </button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.login{
+    text-align: center;
+}
+    
+</style>
