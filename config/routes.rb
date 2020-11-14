34ded3e435a3e1997b3ff72195f8fd0f14b7b6d9
Arnaud <mrandriamihaja2@gmail.com>
2020-11-14 20:05:14 +0300

    route page default

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'site/index'
   get 'site/login'
+  get '/', to: 'site#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
