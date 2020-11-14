59c86734840d2e8cc50002a3ccd8a8dcdb01ba06
Arnaud <mrandriamihaja2@gmail.com>
2020-11-14 09:08:30 +0300

    show hotel

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,67 +1,103 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
-     <div>
-      <input
-        type="text"
-        placeholder="Search"
-        v-model="search"
-        @change="hotelsFilter"
-      />
-    <span v-if="search !=''" class="resulta" > {{hotelsFilter().length}} resultats{{hotelsFilter().length>1 ? 's' :'' }} </span>
+    <div v-show="showH">
+      <p>{{ message }}</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          v-model="search"
+          @change="hotelsFilter"
+        />
+        <span v-if="search != ''" class="resulta">
+          {{ hotelsFilter().length }} resultats{{
+            hotelsFilter().length > 1 ? "s" : ""
+          }}
+        </span>
+      </div>
+      <div
+        class="list_hotel"
+        v-for="(hotel, index) in hotelsFilter()"
+        :key="index"
+      >
+        <div style="display: inline-flex">
+          <div>
+            <Hotel :hotel="hotel" />
+            <div class="btn_options">
+              <button class="btn btn-sm btn-info" @click="show(hotel)">
+                show
+              </button>
+              <!-- <a class="btn btn" @click="addToCart(hotel)">
+          reserve
+          </a> -->
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="list_hotel" v-for="(hotel, index) in hotelsFilter()" :key="index" >
-      <Hotel :hotel = "hotel" />
+    <div v-if="showH == false">
+      <ShowHotel :hotel="hotel" />
+      <div class="btn_options">
+        <button class="btn btn-sm btn-info" @click="show(hotel)">Listes Hotels</button>
+        <!-- <a class="btn btn" @click="addToCart(hotel)">
+          reserve
+          </a> -->
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Hotel from './components/Hotel'
+import ShowHotel from "./components/ShowHotel";
+import axios from "axios";
+import Hotel from "./components/Hotel";
 export default {
   data: function () {
     return {
       message: "Hello Vue God!",
-      hotels:[],
-      hotels_: [],
-      search: ''
-    }
+      hotels: [],
+      showH: true,
+      search: "",
+      hotel: "",
+    };
   },
-  components:{
-    Hotel
+  components: {
+    Hotel,
+    ShowHotel,
   },
-  methods:{
+  methods: {
     hotelsFilter() {
-      console.log(this.search,this.hotels);
-      var _hotels = this.hotels
+      console.log(this.search, this.hotels);
+      var _hotels = this.hotels;
       if (this.search != "") {
         _hotels = this.hotels.filter((hotel) => {
           return hotel.name.toLowerCase().includes(this.search);
         });
-        console.log('recherche', _hotels);
-        return  _hotels;
+        console.log("recherche", _hotels);
+        return _hotels;
       } else {
         return this.hotels;
       }
     },
 
+    show(hotel) {
+      this.hotel = hotel;
+      this.showH = !this.showH;
+    },
   },
-  mounted () {
+  mounted() {
     axios
-      .get('http://127.0.0.1:3000/v1/hotels')
-      .then(response => {
-        this.hotels = response.data
-        console.log(response.data)
+      .get("http://127.0.0.1:3000/v1/hotels")
+      .then((response) => {
+        this.hotels = response.data;
+        console.log(response.data);
       })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-  }
-
-}
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      });
+  },
+};
 </script>
 
 <style scoped>
@@ -78,9 +114,16 @@ p {
   float: left;
   /* width: 33%; */
   display: contents;
-}.resulta{
+}
+.resulta {
   padding-left: 12px;
   color: gray;
   font-size: 18px;
+}
+.btn_options {
+  background-color: grey;
+  margin-left: 2px;
+  margin-right: 2px;
+  text-align: end;
 }
 </style>
--- app/javascript/components/ShowHotel.vue
+++ app/javascript/components/ShowHotel.vue
@@ -1,26 +1,25 @@
 <template>
-     
-    <div class="container_hotel">
-      <div class="hotel">
-        <p class="name_hotel">{{ hotel.name }}</p>
-        <span class="options">{{ hotel.adress }}</span>
-        <span class="options">{{ hotel.specialty }}</span>
-        <img class="image_hotel" :src="hotel.photo" :alt="'image100x100 '+hotel.photo" />
-        <span class="options">{{ hotel.prix }} $</span>
-     
+  <div style="text-align: center;margin: 20px">
+    <div class="container_h">
+      <div class="h">
+        <p class="name_h">{{ hotel.name }}</p>
+        <span class="o">{{ hotel.adress }}</span>
+        <span class="o">{{ hotel.specialty }}</span>
+        <img
+          class="image_h"
+          :src="hotel.photo"
+          :alt="'image100x100 ' + hotel.photo"
+        />
+        <span class="prix">{{ hotel.prix }} $</span>
       </div>
- 
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-    props:['hotel'],
-    components:{
-      
-    }
-}
+  props: ["hotel"],
+};
 </script>
 <style>
 .hotel {
@@ -32,11 +31,9 @@ export default {
   margin: auto;
   display: inline-grid;
 }
-.image_hotel {
-    background-color:grey;
-    min-height: 100px;
-    min-width: 100px;
-  max-width: 200px;
+.image_h {
+  background-color: grey;
+
   vertical-align: middle;
   font-family: Verdana, sans-serif;
   font-size: 15px;
@@ -45,16 +42,19 @@ export default {
   box-sizing: inherit;
   border-style: none;
   vertical-align: middle;
-  width: 204px;
   height: auto;
+}.prix{
+    display: block;
+    color: lawngreen;
+    font-size: 20px;
 }
-.name_hotel {
-  display: inline;
+.name_h {
+  display: block;
   background-color: rgb(110, 110, 110);
 
   text-align: center;
   font-family: Verdana, sans-serif;
-  font-size: 15px;
+  font-size: 20px;
   line-height: 1.5;
   color: rgb(252, 252, 252) !important;
   box-sizing: inherit;
@@ -63,8 +63,9 @@ export default {
   display: block;
   padding: 2px;
 }
-.options {
-  text-align: end;
+.o {
+  display: block;
+  text-align: center;
 }
 .a {
   border: 1px solid blue;
