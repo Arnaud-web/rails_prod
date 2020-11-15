cdd10a5efef754a443c32ee943e68535113cb7b5
Arnaud <mrandriamihaja2@gmail.com>
2020-11-15 06:42:06 +0300

    reconfigure cors

---
+++ app/javascript/components/hotel/Comments.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            <input type="text" placeholder="Commentaire" >
+            <button>Commenter</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
